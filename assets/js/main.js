cf734e47f3ae7da06ef1d9e575be3b93f9c50550
tjramos <tim@theconversionguy.com>
2019-05-27 23:00:14 -0700

    2019 import template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,174 +1,146 @@
 /*
-	Eventually by Pixelarity
-	pixelarity.com @pixelarity
+	Reflex by Pixelarity
+	pixelarity.com | hello@pixelarity.com
 	License: pixelarity.com/license
 */
 
-(function() {
+(function($) {
 
-	"use strict";
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 360px)'
+	});
 
-	// Methods/polyfills.
+	$(function() {
 
-		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
+		var	$window = $(window),
+			$body = $('body');
 
-		// canUse
-			window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
 
-		// window.addEventListener
-			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
 
-	// Vars.
-		var	$body = document.querySelector('body');
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
 
-	// Disable animations/transitions until everything's loaded.
-		$body.classList.add('is-loading');
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
 
-		window.addEventListener('load', function() {
-			window.setTimeout(function() {
-				$body.classList.remove('is-loading');
-			}, 100);
-		});
+		// Menu.
+			var $menu = $('#menu'),
+				$menuInner;
 
-	// Slideshow Background.
-		(function() {
+			$menu.wrapInner('<div class="inner"></div>');
+			$menuInner = $menu.children('.inner');
+			$menu._locked = false;
 
-			// Settings.
-				var settings = {
+			$menu._lock = function() {
 
-					// Images (in the format of 'url': 'alignment').
-						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
-						},
+				if ($menu._locked)
+					return false;
 
-					// Delay.
-						delay: 6000
+				$menu._locked = true;
 
-				};
+				window.setTimeout(function() {
+					$menu._locked = false;
+				}, 350);
 
-			// Vars.
-				var	pos = 0, lastPos = 0,
-					$wrapper, $bgs = [], $bg,
-					k, v;
+				return true;
 
-			// Create BG wrapper, BGs.
-				$wrapper = document.createElement('div');
-					$wrapper.id = 'bg';
-					$body.appendChild($wrapper);
+			};
 
-				for (k in settings.images) {
+			$menu._show = function() {
 
-					// Create BG.
-						$bg = document.createElement('div');
-							$bg.style.backgroundImage = 'url("' + k + '")';
-							$bg.style.backgroundPosition = settings.images[k];
-							$wrapper.appendChild($bg);
+				if ($menu._lock())
+					$menu.addClass('visible');
 
-					// Add it to array.
-						$bgs.push($bg);
+			};
 
-				}
+			$menu._hide = function() {
 
-			// Main loop.
-				$bgs[pos].classList.add('visible');
-				$bgs[pos].classList.add('top');
+				if ($menu._lock())
+					$menu.removeClass('visible');
 
-				// Bail if we only have a single BG or the client doesn't support transitions.
-					if ($bgs.length == 1
-					||	!canUse('transition'))
-						return;
+			};
 
-				window.setInterval(function() {
+			$menu._toggle = function() {
 
-					lastPos = pos;
-					pos++;
+				if ($menu._lock())
+					$menu.toggleClass('visible');
 
-					// Wrap to beginning if necessary.
-						if (pos >= $bgs.length)
-							pos = 0;
+			};
 
-					// Swap top images.
-						$bgs[lastPos].classList.remove('top');
-						$bgs[pos].classList.add('visible');
-						$bgs[pos].classList.add('top');
+			$menuInner
+				.on('click', function(event) {
+					event.stopPropagation();
+				})
+				.on('click', 'a', function(event) {
 
-					// Hide last image after a short delay.
+					var href = $(this).attr('href');
+
+					event.preventDefault();
+					event.stopPropagation();
+
+					// Hide.
+						$menu._hide();
+
+					// Redirect.
 						window.setTimeout(function() {
-							$bgs[lastPos].classList.remove('visible');
-						}, settings.delay / 2);
+							window.location.href = href;
+						}, 250);
 
-				}, settings.delay);
+				});
 
-		})();
+			$menu
+				.appendTo($body)
+				.on('click', function(event) {
 
-	// Signup Form.
-		(function() {
+					event.stopPropagation();
+					event.preventDefault();
 
-			// Vars.
-				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
-					$message;
+					$menu.removeClass('visible');
 
-			// Bail if addEventListener isn't supported.
-				if (!('addEventListener' in $form))
-					return;
+				})
+				.append('<a class="close" href="#menu">Close</a>');
 
-			// Message.
-				$message = document.createElement('span');
-					$message.classList.add('message');
-					$form.appendChild($message);
+			$body
+				.on('click', 'a[href="#menu"]', function(event) {
 
-				$message._show = function(type, text) {
+					event.stopPropagation();
+					event.preventDefault();
 
-					$message.innerHTML = text;
-					$message.classList.add(type);
-					$message.classList.add('visible');
+					// Toggle.
+						$menu._toggle();
 
-					window.setTimeout(function() {
-						$message._hide();
-					}, 3000);
+				})
+				.on('click', function(event) {
 
-				};
+					// Hide.
+						$menu._hide();
 
-				$message._hide = function() {
-					$message.classList.remove('visible');
-				};
+				})
+				.on('keydown', function(event) {
 
-			// Events.
-		// 	// Note: If you're *not* using AJAX, get rid of this event listener.
-		// 		$form.addEventListener('submit', function(event) {
+					// Hide on escape.
+						if (event.keyCode == 27)
+							$menu._hide();
 
-		// 			event.stopPropagation();
-		// 			event.preventDefault();
+				});
 
-		// 			// Hide message.
-		// 				$message._hide();
+	});
 
-		// 			// Disable submit.
-		// 				$submit.disabled = true;
-
-		// 			// Process form.
-		// 			// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-		// 			// but there's enough here to piece together a working AJAX submission call that does.
-		// 				window.setTimeout(function() {
-
-		// 					// Reset form.
-		// 						$form.reset();
-
-		// 					// Enable submit.
-		// 						$submit.disabled = false;
-
-		// 					// Show message.
-		// 						$message._show('success', 'Thank you!');
-		// 						//$message._show('failure', 'Something went wrong. Please try again.');
-
-		// 				}, 750);
-
-		// 		});
-
-		// })();
-
-})();
+})(jQuery);
